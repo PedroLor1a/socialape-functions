144639948440de5dbdc7f875cccb803516216f02
PedroLor1a <loriapedro0@gmail.com>
2024-03-26 20:02:21 -0300

    deploy and notifications

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -118,10 +118,29 @@ exports.getAuthUser = (req, res) => {
       }
     })
     .then((data) => {
-      console.log(data, "ESTO ES DATA");
       userData.likes = [];
       data.forEach((doc) => {
         userData.likes.push(doc.data());
+      });
+      return db
+        .collection("notifications")
+        .where("recipient", "==", req.user.name)
+        .orderBy("createdAt", "desc")
+        .limit(10)
+        .get();
+    })
+    .then((data) => {
+      userData.notifications = [];
+      data.forEach((doc) => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id,
+        });
       });
       return res.json(userData);
     })
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
 const functions = require("firebase-functions");
-const { getAllScreams, postScreams } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postScreams,
+  getScream,
+  commentOnScream,
+  unlikeScream,
+  likeScream,
+  deleteScream,
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -9,17 +17,92 @@ const {
 } = require("./handlers/users");
 const express = require("express");
 const FBAuth = require("./util/fbAuth");
+const { db } = require("./util/admin");
 
 const app = express();
-
+//screams routes
 app.get("/screams", getAllScreams);
 app.post("/createScreams", FBAuth, postScreams);
+app.get("/scream/:screamId", getScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+
+//users routes
+app.post("/signup", signup);
+app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/newDetails", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthUser);
 
-//SignUp Route
-app.post("/signup", signup);
-app.post("/login", login);
-
 exports.api = functions.region("southamerica-east1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("southamerica-east1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/Screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions
+  .region("southamerica-east1")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("southamerica-east1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/Screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
